2ac80ff05204d79165364ff4737381353c67bb16
Асус <https://github.com/Ushiramaru/OOP_1.git>
2019-10-16 03:48:15 +0300

    Added Enum generator

---
+++ Faker/Generator/EnumGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Faker.Generator
+{
+    public class EnumGenerator<T> : IGenerator
+    {
+        private static readonly Random Random = new Random();
+        public object Generate()
+        {
+            var type = typeof(T);
+            var values = Enum.GetValues(type);
+            return values.GetValue(Random.Next(1, values.Length));
+        }
+
+        public Type GetGenerationType()
+        {
+            return typeof(T);
+        }
+    }
+}

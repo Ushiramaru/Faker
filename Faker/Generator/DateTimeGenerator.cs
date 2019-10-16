133c7d9eabc8cf41cd9c43134ca25344ae0c3aea
Асус <https://github.com/Ushiramaru/OOP_1.git>
2019-10-16 03:13:51 +0300

    Added DateTime generator

---
+++ Faker/Generator/DateTimeGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Faker.Generator
+{
+    public class DateTimeGenerator : IGenerator
+    {
+        private static readonly Random Random = new Random();
+        public object Generate()
+        {
+            var buffer = new byte[8];
+            long ticks;
+            do
+            { 
+                Random.NextBytes(buffer); 
+                ticks = BitConverter.ToInt64(buffer, 0);
+            } while (ticks <= DateTime.MinValue.Ticks || ticks >= DateTime.MaxValue.Ticks);
+
+            return new DateTime(ticks);
+        }
+
+        public Type GetGenerationType()
+        {
+            return typeof(DateTime);
+        }
+    }
+}
